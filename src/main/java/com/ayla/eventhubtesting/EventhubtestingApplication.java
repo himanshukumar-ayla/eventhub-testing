29e0a3e8b05d0c6856a00150d6dd9d9378707932
Himanshu Kumar <himanshu.kumar@aylanetworks.com>
2021-02-16 15:25:27 +0530

    first commit

---
+++ src/main/java/com/ayla/eventhubtesting/EventhubtestingApplication.java
@@ -0,0 +1,13 @@
+package com.ayla.eventhubtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventhubtestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventhubtestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ayla/eventhubtesting/EventhubtestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayla.eventhubtesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventhubtestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
